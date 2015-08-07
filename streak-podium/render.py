8ad4850941e299d9dad02cac0e300dc2021b81be
Mitch LeBlanc <supermitch@gmail.com>
2015-08-07 07:46:58 -0700

    Rename svg output based on sort attribute

--- streak-podium/render.py
+++ streak-podium/render.py
@@ -1,14 +1,14 @@
 import pygal
 
 
-def horizontal_bar(sorted_streaks, sort_attrib):
+def horizontal_bar(sorted_streaks, sort):
     """
     Render a horizontal bar chart of streaks.
 
-    Values have already been sorted by sort_attrib.
+    Values have already been sorted by sort.
     """
     users = [user for user, _ in sorted_streaks][::-1]
-    streaks = [getattr(streak, sort_attrib) for _, streak in sorted_streaks][::-1]
+    streaks = [getattr(streak, sort) for _, streak in sorted_streaks][::-1]
 
     chart = pygal.HorizontalStackedBar(show_y_labels=False,
                                        show_x_labels=False,
@@ -16,7 +16,7 @@ def horizontal_bar(sorted_streaks, sort_attrib):
                                        print_values=True,
                                        print_zeroes=False,
                                        print_labels=True)
-    chart.title = 'Top contributors by {} streak'.format(sort_attrib)
+    chart.title = 'Top contributors by {} streak'.format(sort)
     chart.x_labels = users
 
     values = []
@@ -31,5 +31,5 @@ def horizontal_bar(sorted_streaks, sort_attrib):
             values.append(0)  # Let zeroes be boring
 
     chart.add('Streaks', values)
-    chart.render_to_file('top.svg')
+    chart.render_to_file('top_{}.svg'.format(sort))
 
