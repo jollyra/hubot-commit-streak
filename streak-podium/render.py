a8f9c8bedae7e1f9a7f62560f1246529fd6209df
Mitch LeBlanc <supermitch@gmail.com>
2015-08-06 20:08:18 -0700

    Add labels to bars

--- streak-podium/render.py
+++ streak-podium/render.py
@@ -8,13 +8,21 @@ def horizontal_bar(sorted_streaks, sort_attrib):
     Values have already been sorted by sort_attrib.
     """
     users = [user for user, _ in sorted_streaks][::-1]
-    values = [getattr(streak, sort_attrib) for _, streak in sorted_streaks][::-1]
+    streaks = [getattr(streak, sort_attrib) for _, streak in sorted_streaks][::-1]
 
     chart = pygal.HorizontalStackedBar(show_legend=False,
                                        print_values=True, print_zeroes=False,
-                                       show_y_guides=False, show_x_guides=False)
+                                       print_labels=True)
     chart.title = 'Top contributors by {} streak'.format(sort_attrib)
     chart.x_labels = users
+
+    values = []
+    for value, user in zip(streaks, users):
+        if value > 0:
+            values.append({'value': value, 'label': user})
+        else:
+            values.append(0)
+
     chart.add('Streaks', values)
     chart.render_to_file('top.svg')
 
