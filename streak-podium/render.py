2352f18400d5b4b36052e04804bd04e32b000cc4
Mitch LeBlanc <supermitch@gmail.com>
2015-08-06 20:28:39 -0700

    Add links to profiles and clean up chart options

--- streak-podium/render.py
+++ streak-podium/render.py
@@ -10,8 +10,11 @@ def horizontal_bar(sorted_streaks, sort_attrib):
     users = [user for user, _ in sorted_streaks][::-1]
     streaks = [getattr(streak, sort_attrib) for _, streak in sorted_streaks][::-1]
 
-    chart = pygal.HorizontalStackedBar(show_legend=False,
-                                       print_values=True, print_zeroes=False,
+    chart = pygal.HorizontalStackedBar(show_y_labels=False,
+                                       show_x_labels=False,
+                                       show_legend=False,
+                                       print_values=True,
+                                       print_zeroes=False,
                                        print_labels=True)
     chart.title = 'Top contributors by {} streak'.format(sort_attrib)
     chart.x_labels = users
@@ -19,9 +22,13 @@ def horizontal_bar(sorted_streaks, sort_attrib):
     values = []
     for value, user in zip(streaks, users):
         if value > 0:
-            values.append({'value': value, 'label': user})
+            values.append({
+                'value': value,
+                'label': user,
+                'xlink': 'https://github.com/{}'.format(user)
+            })
         else:
-            values.append(0)
+            values.append(0)  # Let zeroes be boring
 
     chart.add('Streaks', values)
     chart.render_to_file('top.svg')
