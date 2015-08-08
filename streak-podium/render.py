1d752a07931e491f1e58c0de632756bddd0b63f1
Mitch LeBlanc <supermitch@gmail.com>
2015-08-08 12:28:47 -0700

    Render SVG using Matplotlib
    No need for pygal now.

--- streak-podium/render.py
+++ streak-podium/render.py
@@ -1,7 +1,5 @@
-import pygal
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def horizontal_bar(sorted_streaks, sort):
@@ -23,28 +21,5 @@ def horizontal_bar(sorted_streaks, sort):
     plt.xlim([0, max(streaks) + 0.5])  # x-limits a bit wider at right
 
     figure.savefig('temp/top_{}.png'.format(sort), format='png')
-
-    # Export SVG using pygal
-    chart = pygal.HorizontalStackedBar(show_y_labels=False,
-                                       show_x_labels=False,
-                                       show_legend=False,
-                                       print_values=True,
-                                       print_zeroes=False,
-                                       print_labels=True)
-    chart.title = title
-    chart.x_labels = users
-
-    values = []
-    for value, user in zip(streaks, users):
-        if value > 0:
-            values.append({
-                'value': value,
-                'label': user,
-                'xlink': 'https://github.com/{}'.format(user)
-            })
-        else:
-            values.append(0)  # Let zeroes be boring
-    chart.add('Streaks', values)
-
-    chart.render_to_file('temp/top_{}.svg'.format(sort))
+    figure.savefig('temp/top_{}.svg'.format(sort), format='svg')
 
