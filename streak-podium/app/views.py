63a5e8a5db222c0dab6dfff166e3757cdb04fc10
Mitch LeBlanc <supermitch@gmail.com>
2015-08-29 19:31:59 -0700

    Add templates and link to Github Auth

--- streak-podium/app/views.py
+++ streak-podium/app/views.py
@@ -1,5 +1,19 @@
+import random
+from string import ascii_letters, digits
+
+from config import config
 from app import app
+
+from flask import render_template
+
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    N = 20
+    rand_str = ''.join(random.SystemRandom().choice(ascii_letters + digits) for _ in range(N))
+    return render_template('index.html',
+                           client_id=config.GITHUB_CLIENT_ID,
+                           auth_url='http://127.0.0.1:5000/auth',
+                           scope='read:org',
+                           state=rand_str)
+
