aa86ef410634da64a46824334661c50ca4045963
Mitch LeBlanc <supermitch@gmail.com>
2015-08-29 21:14:29 -0700

    Fix wrong code flow to handle response - DRAFT

--- streak-podium/app/fetch.py
+++ streak-podium/app/fetch.py
@@ -8,15 +8,25 @@ from config import config
 
 
 def post_temporary_code(code, state):
+    """ POST code & state to Github to get an access token. """
+
+    print('POSTING code and state to Github')
+
     url = 'https://github.com/login/oauth/access_token'
     payload = {
         'client_id': config.GITHUB_CLIENT_ID,
         'client_secret': config.GITHUB_CLIENT_SECRET,
         'code': code,
-        'redirect_uri': url_for('auth'),
+        'redirect_uri': url_for('auth', _external=True),
         'state': state,
     }
-    r = requests.post(url, data=json.dumps(payload))
-    if not r.status_code == requests.codes.ok:
-        logging.info('Full authorization succeeded')
+    r = requests.post(url, data=payload)
+    print(r.status_code)
+    print(r.content)
+    if r.status_code == requests.codes.ok:
+        print('Response: {} -- Full authorization succeeded'.format(r.status_code))
+        return True
+    else:
+        print('Response: {} -- Full authorization failed'.format(r.status_code))
+        return False
 
--- streak-podium/app/views.py
+++ streak-podium/app/views.py
@@ -25,32 +25,44 @@ def index():
 
 @app.route('/auth')
 def auth():
-    if 'success_token' in request.args:
-        success_token = request.args['success_token']
-        scope = request.args['success_token']
-        token_type = request.args['success_token']
-
-        root = os.path.dirname(os.path.abspath(__file__))
-        out_file = os.path.join(root, 'auth.json')
-        with open(out_file, 'w') as f:  # TODO: Persist response in DB
-            f.write('SUCCESS_TOKEN {}\nSCOPE {}\nTOKEN_TYPE {}\n'
-                    ''.format(success_token, scope, token_type))
-
-        return redirect(url_for('success'))
-
-    elif 'code' in request.args:
+    if 'code' in request.args:
         code = request.args['code']
         state = request.args['state']
-        logging.debug('CODE', code)
-        logging.debug('STATE', state)  # TODO: Compare state to stored SESSION variable
+        print('CODE', code)
+        print('STATE', state)  # TODO: Compare state to stored SESSION variable
 
-        fetch.post_temporary_code(code, state)
+        response = fetch.post_temporary_code(code, state)
 
-        return 'Sending temporary code for confirmation...'
+        if response.status_code == request.codes.success:
+            print(response.content)
+            success_token = request.args['success_token']
+            scope = request.args['success_token']
+            token_type = request.args['success_token']
+
+            print('SUCCESS_TOKEN', success_token)
+            print('SCOPE', scope)
+            print('TOKEN_TYPE', token_type)
+
+            root = os.path.dirname(os.path.abspath(__file__))
+            out_file = os.path.join(root, 'auth.json')
+            with open(out_file, 'w') as f:  # TODO: Persist response in DB
+                f.write('SUCCESS_TOKEN {}\nSCOPE {}\nTOKEN_TYPE {}\n'
+                        ''.format(success_token, scope, token_type))
+
+            return redirect(url_for('success'))
+        else:
+            return redirect(url_for('failure'))
 
     else:
         return 'Invalid request'
 
+
 @app.route('/success')
 def success():
     return render_template('success.html')
+
+
+@app.route('/failure')
+def failure():
+    return render_template('failure.html')
+
